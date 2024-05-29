6fbd1f7ad70574ca01b8b1ae8dcf332ed87a7ce1
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-05-29 14:14:41 +0300

    initial commit + core data

---
+++ AuthApp/AuthApp/AuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppApp.swift
+//  AuthApp
+//
+//  Created by Alexandr Filovets on 29.05.24.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
